26bb5f307c24760f47c3cb4cfb4f11c773ee543f
RamadanRangkuti <ramadanrangkuti17@gmail.com>
2023-09-28 12:33:43 +0700

    adjusment deploy

--- helpers/connection.js
+++ helpers/connection.js
@@ -10,6 +10,13 @@ const db = new Client({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
 })
+// const db = new Client({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT
+// })
 db.connect((err) => {
   if (err) {
     console.log('db connection error', err)
