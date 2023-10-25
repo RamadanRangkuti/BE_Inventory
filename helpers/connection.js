06ae2f910fc9510fcaf2a5e4f9a81cad398ce3a1
RamadanRangkuti <ramadanrangkuti17@gmail.com>
2023-10-25 12:22:31 +0700

    bcrypt update

--- helpers/connection.js
+++ helpers/connection.js
@@ -3,20 +3,20 @@ require('dotenv').config({ path: '../.env' })
 
 // Des
 // const { USER, HOST, DATABASE, PORT } = process.env
-const db = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-})
 // const db = new Client({
-//   user: process.env.USER,
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORT
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT
 // })
+const db = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
+})
 db.connect((err) => {
   if (err) {
     console.log('db connection error', err)
